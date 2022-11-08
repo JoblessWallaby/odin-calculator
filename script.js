334a07ef158e47cb84bb6f45a31498fbf1fa56c2
HelloImSleepy <helloitssleepy@gmail.com>
2022-11-07 18:20:29 -0800

    feat: add working calculator through console

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 
+let num1 = prompt('Please choose a number');
+let num2 = prompt('Please choose a number');
+let operator = prompt('Please choose an operator')
+
+operate(operator, num1, num2);
 
 
-
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case '+':
+      add(num1,num2);
+      break;
+    case '-':
+      subtract(num1,num2);
+      break;
+    case '*':
+      multiply(num1,num2);
+      break;
+    case '/':
+      divide(num1,num2);
+      break;
+  }
+}
 
 
 function add(num1, num2) {
   let sum = (num1 + num2);
-  return sum;
+  console.log(sum);
 }
 
 function subtract(num1, num2) {
   let total = (num1 - num2);
-  return total;
+  console.log(total);
 }
 
 function multiply(num1, num2) {
  let total = (num1 * num2);
- return total;
+ console.log(total);
 }
 
 function divide(num1, num2) {
   let total = (num1 / num2);
-  return total;
+  console.log(total);
 }
