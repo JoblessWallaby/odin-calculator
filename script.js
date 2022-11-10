17ce4d22cac743b458d184916ca19978737b55f1
HelloImSleepy <helloitssleepy@gmail.com>
2022-11-09 23:07:31 -0800

    feat: add delete, allClear, equal functions

--- script.js
+++ script.js
@@ -7,14 +7,20 @@ function operate(operator, num1, num2) {
     case '-':
       subtract(num1,num2);
       break;
-    case '*':
+    case '×':
       multiply(num1,num2);
       break;
-    case '/':
+    case '÷':
       divide(num1,num2);
       break;
   }
 }*/
+
+let num1=''
+let tempNum1 =''
+let total =''
+let operator ='';
+
 
 //Allows numbers to get displayed on calculator
 const numberButton = document.querySelectorAll('.number');
@@ -23,14 +29,54 @@ const userInput = document.querySelector('.userInput');
 numberButton.forEach((button => {
   button.addEventListener('click', function(e) {
     userInput.append(button.textContent);
+    num1 = userInput.textContent;
+    
   })
 }))
+
+//Allows number to get displayed
+const equal = document.querySelector('.equal');
+const result = document.querySelector('.result');
+
+equal.addEventListener('click', function(e) {
+  total = num1;
+  result.append(total);
+})
+
+//All Clear function
+const display = document.querySelector('.display');
+const allClear = document.querySelector('.allClear')
+
+allClear.addEventListener('click', (e) => {
+  location.reload()
+})
+
+//Delete function
+const del = document.querySelector('.delete');
+
+del.addEventListener('click', (e) => {
+  num1 = num1.slice(0, -1);
+  userInput.textContent = num1;
+})
+
+
+//operator save function
+const operand = document.querySelectorAll('.operand');
+
+operand.forEach((button) => {
+  button.addEventListener('click', (e)=> {
+    userInput.append(button.textContent);
+    num1 = tempNum1;
+    operator = button.textContent;
+    console.log(operator)
+  })
+})
 
 
 //Add function
 function add(num1, num2) {
-  let sum = (num1 + num2);
-  console.log(sum);
+  let total = (num1 + num2);
+  console.log(total);
 }
 //Subtract function
 function subtract(num1, num2) {
