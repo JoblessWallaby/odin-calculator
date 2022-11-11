51a1d3828ead10731e996708355544e98d4b6b17
HelloImSleepy <helloitssleepy@gmail.com>
2022-11-10 21:06:11 -0800

    feat: add basic functionality

--- script.js
+++ script.js
@@ -1,96 +1,103 @@
-/*
+
+let num1 = '';
+let num2 = '';
+let tempNum1 = '';
+let operator ='';
+let previousOperator = '';
+let dotClick = false;
+
+
+
+const userInput = document.querySelector('.userInput');
+const result = document.querySelector('.result');
+const numberButton = document.querySelectorAll('.number');
+const operandButton = document.querySelectorAll('.operand');
+const equal = document.querySelector('.equal');
+
+
+numberButton.forEach((numButton) => {
+  numButton.addEventListener('click', (e) => {
+    if(e.target.innerText === '.' && !dotClick) {
+      dotClick = true;
+    } else if (e.target.innerText === '.' && dotClick){
+      return;
+    }
+    tempNum1 += e.target.innerText;
+    userInput.innerText = tempNum1;
+
+    if(num1 !== '') {
+      num2 = parseFloat(tempNum1);
+    }
+
+    if (num1 && num2 && operator) {
+      operate(operator, num1, num2)
+    }
+  })
+})
+
+
+operandButton.forEach((operatorButton) => {
+  operatorButton.addEventListener('click', (e) => {
+    num1 = parseFloat(tempNum1);
+    tempNum1 = '';
+    operator = operatorButton.textContent;
+  })
+
+})
+
+
+
+equal.addEventListener('click', (e) => {
+  console.log(`Num 1: ${num1} Num 2: ${num2} Operand: ${operator}`)
+})
+
+//Operate Function
 function operate(operator, num1, num2) {
   switch (operator) {
     case '+':
-      add(num1,num2);
+      add(num1, num2);
       break;
     case '-':
-      subtract(num1,num2);
+      subtract(num1, num2);
       break;
     case '×':
-      multiply(num1,num2);
+      multiply(num1, num2);
       break;
     case '÷':
       divide(num1,num2);
       break;
+    case '%':
+      modulus(num1, num2);
+      break;
   }
-}*/
+}
 
-let num1=''
-let tempNum1 =''
-let total =''
-let operator ='';
-
-
-//Allows numbers to get displayed on calculator
-const numberButton = document.querySelectorAll('.number');
-const userInput = document.querySelector('.userInput');
-
-numberButton.forEach((button => {
-  button.addEventListener('click', function(e) {
-    userInput.append(button.textContent);
-    num1 = userInput.textContent;
-    
-  })
-}))
-
-//Allows number to get displayed
-const equal = document.querySelector('.equal');
-const result = document.querySelector('.result');
-
-equal.addEventListener('click', function(e) {
-  total = num1;
-  result.append(total);
-})
-
-//All Clear function
-const display = document.querySelector('.display');
-const allClear = document.querySelector('.allClear')
-
-allClear.addEventListener('click', (e) => {
-  location.reload()
-})
-
-//Delete function
-const del = document.querySelector('.delete');
-
-del.addEventListener('click', (e) => {
-  num1 = num1.slice(0, -1);
-  userInput.textContent = num1;
-})
-
-
-//operator save function
-const operand = document.querySelectorAll('.operand');
-
-operand.forEach((button) => {
-  button.addEventListener('click', (e)=> {
-    userInput.append(button.textContent);
-    num1 = tempNum1;
-    operator = button.textContent;
-    console.log(operator)
-  })
-})
-
-
-//Add function
-function add(num1, num2) {
+//Add Function 
+function add (num1,num2) {
   let total = (num1 + num2);
-  console.log(total);
-}
-//Subtract function
-function subtract(num1, num2) {
-  let total = (num1 - num2);
-  console.log(total);
-}
-//Multiply function
-function multiply(num1, num2) {
- let total = (num1 * num2);
- console.log(total);
-}
-//Divide function
-function divide(num1, num2) {
-  let total = (num1 / num2);
-  console.log(total);
+  console.log(total)
 }
 
+//Subtract Function
+function subtract (num1,num2) {
+  let total = (num1 - num2);
+  console.log(total)
+}
+
+//Multiply Function
+function multiply (num1,num2) {
+  let total = (num1 * num2);
+  console.log(total)
+}
+
+//Divide Function
+function divide (num1,num2) {
+  let total = (num1 / num2);
+  console.log(total)
+}
+
+//Modulus Function
+function modulus (num1,num2) {
+  let total = (num1 % num2);
+  console.log(total)
+}
